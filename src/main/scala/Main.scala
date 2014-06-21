f9fe3e2ca36e7df14d3659eba92102f18641e9de
Adam Bergmark <adam@bergmark.nl>
2014-06-21 22:50:08 +0200

    Separate side effects from pure functions and add more images

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,16 +12,12 @@ case class Job (filePath : String, plate : String) {
 
 }
 
-object Data {
-  val jobs : HashMap[String,Job] = HashMap(
-      "bulbasaur" -> Job("001-bulbasaur-40x50.png"  , "big"  )
-    , "growlite"  -> Job("058-growlite-48x48.png"   , "small")
-    , "dragonite" -> Job("149-dragonite-unsized.png", "big"  )
-    , "walter"    -> Job("walter.png", "small")
-    )
+object IO {
+  import Strings._
+  import Colors._
 
-  def readColors(job : Job) : Array[Array[Color]] = {
-    val fp = Main.resourceFromClassloader(job.filePath).toURI()
+  def readColors(path: String): Array[Array[Color]] = {
+    val fp = getFile(path)
     println("Reading " + fp)
     val img = ImageIO.read(new File(fp))
     val raster = img.getData
@@ -31,21 +27,52 @@ object Data {
     Array.tabulate(w, h) { (x, y) => new Color(img.getRGB(x, y))}
   }
 
+  def readPHColors: HashMap[Color, String] = {
+    val source = Source.fromFile(getFile("colors.txt"))
+    val lines = source.mkString
+    source.close()
+    lines.split("\n").map { v => v.split("=")}.map { v => (v(0), Color.decode("#" + v(1)))}.foldLeft(new HashMap[Color, String]) { case (h, (s, c)) => h + (c -> s)}
+  }
+
   def getFile (name : String) : URI = {
     Main.resourceFromClassloader(name).toURI
   }
 
-
-  def readPHColors : HashMap[Color,String] = {
-    val source = Source.fromFile(getFile("colors.txt"))
-    val lines = source.mkString
-    source.close
-    lines.split("\n").map { v => v.split("=") }.map { v => (v(0), Color.decode("#" + v(1))) }.foldLeft(new HashMap[Color,String]) { case (h,(s,c)) => h + (c -> s) }
+  def printBuildPlan(phColors : HashMap[Color,String], img : Array[Array[Color]]) : Array[Array[Color]] = {
+    countColors(img).foreach { case (c:Color, i:Int) =>
+      println(
+        phColors(c)
+          + " (" + padL(c.getRed.toString  ,3)
+          + ","  + padL(c.getGreen.toString,3)
+          + ","  + padL(c.getBlue.toString ,3)
+          + ")"
+          + ", "
+          + padL(i.toString,4) + "px, "
+          + padR((i.toFloat/140).toString,11)
+          + " ~ "
+          + Math.ceil(i.toFloat/140) + " squares"
+      )
+    }
+    println("total pixels = " + countColors(img).foldLeft(0) { case (acc,(_,i)) => acc + i })
+    img
   }
 
-  val phColors = readPHColors
+}
+
+object Config {
+  val jobs : HashMap[String,Job] = HashMap(
+      "bulbasaur" -> Job("001-bulbasaur-40x50.png", "big")
+    , "growlite"  -> Job("058-growlite-48x48.png", "small")
+    , "dragonite" -> Job("149-dragonite-unsized.png", "big")
+    , "walter"    -> Job("walter.png", "small")
+  )
+  val plates = HashMap[String,Tuple2[Int,Int]]("small" -> Tuple2(24, 24), "big" -> Tuple2(40, 50), "biglandscape" -> Tuple2(50,40))
+}
+
+object Colors {
 
   val yuvMatrix : Array[Array[Double]] = Array(Array(0.299,0.587,0.114),Array(-0.14713,-0.28886,0.436),Array(0.615,-0.51499,-0.10001))
+
   def toYuv(c : Color) : Array[Double] = {
     val r : Array[Double] = Array(c.getRed.toDouble, c.getGreen.toDouble, c.getBlue.toDouble)
     Matrix.multiply(yuvMatrix, r)
@@ -65,25 +92,7 @@ object Data {
     cs.minBy { c2 => Math.abs(c.getRed - c2.getRed) + Math.abs(c.getGreen - c2.getGreen) + Math.abs(c.getBlue - c2.getBlue) }
   }
 
-  def g(c:Color,i:Int) {
-  }
-
-  def padL(s : String, n : Int) : String = {
-    var res = s
-    while (res.length < n) {
-      res = " " + res
-    }
-    res
-  }
-  def padR(s : String, n : Int) : String = {
-    var res = s
-    while (res.length < n) {
-      res = res + " "
-    }
-    res
-  }
-
-  def makeYUV(img : Array[Array[Color]]) : Array[Array[Color]] = {
+  def makeYUV(phColors : HashMap[Color,String], img : Array[Array[Color]]) : Array[Array[Color]] = {
     img.map { r => r.map { c => findClosestColorYUV(c, phColors.keys.toArray) } }
   }
 
@@ -101,26 +110,7 @@ object Data {
     }
   }
 
-  def printBuildPlan(img : Array[Array[Color]]) : Array[Array[Color]] = {
-    countColors(img).foreach { case (c:Color, i:Int) =>
-      println(
-        phColors(c)
-          + " (" + padL(c.getRed.toString  ,3)
-          + ","  + padL(c.getGreen.toString,3)
-          + ","  + padL(c.getBlue.toString ,3)
-          + ")"
-          + ", "
-          + padL(i.toString,4) + "px, "
-          + padR((i.toFloat/140).toString,11)
-          + " ~ "
-          + Math.ceil(i.toFloat/140) + " squares"
-      )
-    }
-    println("total pixels = " + countColors(img).foldLeft(0) { case (acc,(_,i)) => acc + i })
-    img
-  }
-
-  def makeRGB(img : Array[Array[Color]]) : Array[Array[Color]] = {
+  def makeRGB(phColors : HashMap[Color,String], img : Array[Array[Color]]) : Array[Array[Color]] = {
     img.map { r => r.map { c => findClosestColorRGB(c, phColors.keys.toArray) } }
   }
 
@@ -128,22 +118,43 @@ object Data {
     img.flatten.foldLeft(new HashMap[Color,Int]) { (h, c) => h + (c -> h.get(c).map(i => i + 1).getOrElse(1)) }
   }
 
-  val plates = HashMap[String,Tuple2[Int,Int]]("small" -> Tuple2(24, 24), "big" -> Tuple2(40, 50), "biglandscape" -> Tuple2(50,40))
+
 }
 
-class DataPanel(log : Log, data : Array[Array[Color]], job : Job, pixelSize : Int) extends Panel {
-  preferredSize = new Dimension(600, 600)
+object Strings {
 
-  val w = pixelSize
-  val h = pixelSize
-  val grid = 1
-  val plateSepWidth = 4
+
+  def padL(s : String, n : Int) : String = {
+    var res = s
+    while (res.length < n) {
+      res = " " + res
+    }
+    res
+  }
+
+  def padR(s : String, n : Int) : String = {
+    var res = s
+    while (res.length < n) {
+      res = res + " "
+    }
+    res
+  }
+
+
+}
+
+class DataPanel(phColors : HashMap[Color,String], log : Log, data : Array[Array[Color]], job : Job, pixelSize : Int) extends Panel {
+  preferredSize      = new Dimension(600, 600)
+  val w              = pixelSize
+  val h              = pixelSize
+  val grid           = 1
+  val plateSepWidth  = 4
   val plateSepHeight = 4
-  val plateWidth  = Data.plates(job.plate)._1
-  val plateHeight = Data.plates(job.plate)._2
-  val rows = data.length
-  val cols = data(1).length
-  val totalWidth = cols*w + ((grid * cols) - 1) + (Math.floor(cols/plateWidth).toInt * plateSepWidth)
+  val plateWidth     = Config.plates(job.plate)._1
+  val plateHeight    = Config.plates(job.plate)._2
+  val rows           = data.length
+  val cols           = data(1).length
+  val totalWidth     = cols*w + ((grid * cols) - 1) + (Math.floor(cols/plateWidth).toInt * plateSepWidth)
 
   override def paintComponent(g : Graphics2D) {
     g.setColor(new Color(0,0,0))
@@ -184,7 +195,7 @@ class DataPanel(log : Log, data : Array[Array[Color]], job : Job, pixelSize : In
         val clrString =  "#"+Integer.toHexString(clr.getRGB).substring(2).toUpperCase
         if (!someEquals(lastLogged, Some(c))) {
           lastLogged = Some(c)
-          log.write(/*e.point.x + ", " + e.point.y + " => " + */ gc.toString + ", " + cellInGrid.toString + ", color: " + Data.phColors.get(clr).getOrElse("N/A") + ", " + clrString)
+          log.write(/*e.point.x + ", " + e.point.y + " => " + */ gc.toString + ", " + cellInGrid.toString + ", color: " + phColors.get(clr).getOrElse("N/A") + ", " + clrString)
           repaint()
         }
       }
@@ -224,7 +235,7 @@ class DataPanel(log : Log, data : Array[Array[Color]], job : Job, pixelSize : In
 
 case class Cell(x : Int, y : Int) {
   override def toString : String = {
-    "Cell(" + Data.padL(x.toString,3) + ", " + Data.padL(y.toString,3) + ")"
+    "Cell(" + Strings.padL(x.toString,3) + ", " + Strings.padL(y.toString,3) + ")"
   }
   override def equals(o : Any) = o match {
     case c:Cell => x == c.x && y == c.y
@@ -234,7 +245,7 @@ case class Cell(x : Int, y : Int) {
 
 case class GridCell(x : Int, y : Int) {
   override def toString : String = {
-    "GridCell(" + Data.padL(x.toString,3) + ", " + Data.padL(y.toString,3) + ")"
+    "Grid(" + Strings.padL(x.toString,3) + ", " + Strings.padL(y.toString,3) + ")"
   }
   override def equals(o : Any) = o match {
     case c:Cell => x == c.x && y == c.y
@@ -250,13 +261,14 @@ class Log extends TextArea(rows=40, columns=40) {
 }
 
 object Main extends SimpleSwingApplication {
-  val pixelSize = 8
-  val job = Data.jobs("growlite")
-  val origColors = Data.readColors(job)
-  val log = new Log
-  val origImg = new DataPanel(log, origColors, job, pixelSize)
-  val yuvImg = new DataPanel(log, Data.printBuildPlan(Data.makeYUV(origColors)), job, pixelSize)
-  def top  = new MainFrame {
+  val pixelSize  = 13
+  val phColors   = IO.readPHColors
+  val job        = Config.jobs("growlite")
+  val origColors = IO.readColors(job.filePath)
+  val log        = new Log()
+  val origImg    = new DataPanel(phColors, log, origColors, job, pixelSize)
+  val yuvImg     = new DataPanel(phColors, log, IO.printBuildPlan(phColors, Colors.makeYUV(phColors, origColors)), job, pixelSize)
+  def top        = new MainFrame {
     title    = "Pixels"
     contents = new GridPanel(2,2) {
       background    = new Color(0, 0,0)
